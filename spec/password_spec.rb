f468d340112075d966575e4116d83af7ffd8b505
Josh Holtz <josh@rokkincat.com>
2021-03-25 03:03:10 -0500

    Separated tests for ascii and non-ascii passwords

--- spec/password_spec.rb
+++ spec/password_spec.rb
@@ -34,24 +34,69 @@ describe InternetPassword do
   describe '#add' do
     let(:server) { 'example.com' }
     let(:account) { 'jappleseed@example.com' }
-    let(:password) { '•••p4ssw0rd!••' }
-    let(:comment) { '•••Some comment•••' }
 
-    around(:example) do |example|
-      InternetPassword.add(server, account, password, comment: comment)
-      example.run
-      InternetPassword.delete({ server: server, account: account })
+    describe 'ascii password' do
+      let(:password) { 'p4ssw0rd!' }
+      let(:comment) { 'Some comment' }
+
+      around(:example) do |example|
+        InternetPassword.add(server, account, password, comment: comment)
+        example.run
+        InternetPassword.delete({ server: server, account: account })
+      end
+
+      it 'should be added to the keychain' do
+        entry = InternetPassword.find({ account: account })
+        expect(entry.keychain.filename).to be == keychain.filename
+        expect(entry.attributes).to include({
+                                              'acct' => account,
+                                              'srvr' => server,
+                                              'icmt' => comment
+                                            })
+        expect(entry.password).to be == password
+      end
     end
 
-    it 'should be added to the keychain' do
-      entry = InternetPassword.find({ account: account })
-      expect(entry.keychain.filename).to be == keychain.filename
-      expect(entry.attributes).to include({
-                                            'acct' => account,
-                                            'srvr' => server,
-                                            'icmt' => comment
-                                          })
-      expect(entry.password).to be == password
+    describe 'ascii password with backslash' do
+      let(:password) { 'p4ssw\0rd!' }
+
+      around(:example) do |example|
+        InternetPassword.add(server, account, password)
+        example.run
+        InternetPassword.delete({ server: server, account: account })
+      end
+
+      it 'should be added to the keychain' do
+        entry = InternetPassword.find({ account: account })
+        expect(entry.keychain.filename).to be == keychain.filename
+        expect(entry.attributes).to include({
+                                              'acct' => account,
+                                              'srvr' => server
+                                            })
+        expect(entry.password).to be == password
+      end
+    end
+
+    describe 'non-ascii password' do
+      let(:password) { '•••p4ssw0rd!••' }
+      let(:comment) { '•••Some comment•••' }
+
+      around(:example) do |example|
+        InternetPassword.add(server, account, password, comment: comment)
+        example.run
+        InternetPassword.delete({ server: server, account: account })
+      end
+
+      it 'should be added to the keychain' do
+        entry = InternetPassword.find({ account: account })
+        expect(entry.keychain.filename).to be == keychain.filename
+        expect(entry.attributes).to include({
+                                              'acct' => account,
+                                              'srvr' => server,
+                                              'icmt' => comment
+                                            })
+        expect(entry.password).to be == password
+      end
     end
   end
 end
